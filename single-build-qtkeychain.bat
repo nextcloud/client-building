35419bb28d101ddbaada95855e17e055819abfde
Kevin Ottens <kevin.ottens@nextcloud.com>
2020-12-09 14:15:51 +0100

    Do the master checkout just before the pull not after
    Signed-off-by: Kevin Ottens <kevin.ottens@nextcloud.com>

--- single-build-qtkeychain.bat
+++ single-build-qtkeychain.bat
@@ -84,6 +84,9 @@ rem Reference: https://ss64.com/nt/setlocal.html
 rem Reference: https://ss64.com/nt/start.html
 
 if "%PULL_QTKEYCHAIN%" == "1" (
+    echo "* go back to master: git checkout master at %MY_REPO%/."
+    start "git checkout master" /D "%MY_REPO%/" /B /wait git checkout master
+
     echo "* git pull at %MY_REPO%/."
     start "git pull" /D "%MY_REPO%/" /B /wait git pull --tags
 )
@@ -109,9 +112,6 @@ if %ERRORLEVEL% neq 0 goto onError
 echo "* Run cmake to compile and install."
 start "cmake build" /D "%MY_BUILD_PATH%" /B /wait cmake --build . --config %BUILD_TYPE% --target install
 if %ERRORLEVEL% neq 0 goto onError
-
-echo "* go back to master: git checkout master at %MY_REPO%/."
-start "git checkout master" /D "%MY_REPO%/" /B /wait git checkout master
 
 echo "* Copy qt5keychain%DLL_SUFFIX%.dll to %QT_BIN_PATH%/ for windeployqt to find it."
 start "copy qt5keychain%DLL_SUFFIX%.dll for windeployqt" /B /wait cp -f "%MY_INSTALL_PATH%/bin/qt5keychain%DLL_SUFFIX%.dll" "%QT_BIN_PATH%/"
