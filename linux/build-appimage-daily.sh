15008a58dc360690fabeb72220f8699882a60006
Felix Weilbach <felix.weilbach@t-online.de>
2021-01-05 15:42:39 +0100

    Remove NO_SHIBBOLETH flag
    Signed-off-by: Felix Weilbach <felix.weilbach@t-online.de>

--- linux/build-appimage-daily.sh
+++ linux/build-appimage-daily.sh
@@ -26,7 +26,7 @@ mkdir build
 cd build
 cmake -D CMAKE_INSTALL_PREFIX=/usr ../
 make -j4
-make install 
+make install
 
 #Build client
 cd /build
@@ -34,7 +34,6 @@ git clone --depth 1 https://github.com/nextcloud/desktop.git
 mkdir build-client
 cd build-client
 cmake -D CMAKE_INSTALL_PREFIX=/usr \
-    -D NO_SHIBBOLETH=1 \
     -D BUILD_UPDATER=ON \
     -D QTKEYCHAIN_LIBRARY=/app/usr/lib/x86_64-linux-gnu/libqt5keychain.so \
     -D QTKEYCHAIN_INCLUDE_DIR=/app/usr/include/qt5keychain/ \
