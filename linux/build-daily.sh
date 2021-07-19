544b8b29b59fe8bc0000399fc122910541f4d7ad
tobiasKaminsky <tobias@kaminsky.me>
2021-07-19 07:46:16 +0200

    Remove all but the latest 5 dailies to save space

--- linux/build-daily.sh
+++ linux/build-daily.sh
@@ -23,3 +23,6 @@ docker rm desktop-$DATE
 
 #Copy to the download server
 scp ~/output/$DATE/*.AppImage daily_desktop_uploader@download.nextcloud.com:/var/www/html/desktop/daily/Linux
+
+# remove all but the latest 5 dailies
+/bin/ls -t ~/output | awk 'NR>6' | xargs rm -fr
