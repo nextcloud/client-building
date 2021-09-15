6fc38f50a72a7a1174d47f588cf01905fd1dd324
Camila <hello@camila.codes>
2021-09-15 17:39:50 +0200

    Fix 'Could not find a configuration file for package "Qt5" that is compatible'.
    Signed-off-by: Camila <hello@camila.codes>

--- linux/build-daily.sh
+++ linux/build-daily.sh
@@ -12,7 +12,7 @@ docker run \
     --name desktop-$DATE \
     -v $DIR:/input \
     -v ~/output/$DATE:/output \
-    ghcr.io/nextcloud/continuous-integration-client-appimage:client-appimage-1 \
+    ghcr.io/nextcloud/continuous-integration-client-appimage:client-appimage-2 \
     /input/build-appimage-daily.sh $(id -u)
 
 #Save the logs!
