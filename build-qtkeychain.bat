861edcefd00c13104ece7f2cbb754b20726438c2
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2023-05-31 19:12:31 +0200

    let the empty fake scripts do what is needed
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- build-qtkeychain.bat
+++ build-qtkeychain.bat
@@ -1,3 +1,4 @@
 echo "build-qtkeychain.bat is obsolete"
+echo "*** Finished Build: qtkeychain %BUILD_TYPE% %BUILD_ARCH% (GIT_REVISION=%GIT_REVISION%) (%~nx0)"
 
 exit 0
--- build-zlib.bat
+++ build-zlib.bat
@@ -1,3 +1,4 @@
 echo "build-zlib.bat is obsolete"
+echo "*** Finished Build: zlib %BUILD_TYPE% %BUILD_ARCH% (GIT_REVISION=%GIT_REVISION%) (%~nx0)"
 
 exit 0
