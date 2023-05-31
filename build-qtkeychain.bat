89d4f33249ab38cb832ca62fa31a5c25fa662fd1
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2023-05-31 19:20:46 +0200

    without echo off, we are not matching expectations of callers
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- build-qtkeychain.bat
+++ build-qtkeychain.bat
@@ -1,3 +1,5 @@
+@echo off
+
 echo "build-qtkeychain.bat is obsolete"
 echo "*** Finished Build: qtkeychain %BUILD_TYPE% %BUILD_ARCH% (GIT_REVISION=%GIT_REVISION%) (%~nx0)"
 
--- build-zlib.bat
+++ build-zlib.bat
@@ -1,3 +1,5 @@
+@echo off
+
 echo "build-zlib.bat is obsolete"
 echo "*** Finished Build: zlib %BUILD_TYPE% %BUILD_ARCH% (GIT_REVISION=%GIT_REVISION%) (%~nx0)"
 
