78fded4f2137033d2d565ce1537c3edb9aa36a35
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2023-06-26 18:16:58 +0200

    define proper 32 or 64 bits build environment
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- common.inc.bat
+++ common.inc.bat
@@ -2,34 +2,17 @@ Rem ****************************************************************************
 rem "common environment Variables"
 Rem ******************************************************************************************
 
-rem Release or Debug
-::set "BUILD_TYPE=Release"
-
-if "%~1" == "Debug" (set "BUILD_TYPE=%~1")
-
-if "%~2" == "Win32" (set "BUILD_ARCH=%~2")
-
-if "%BUILD_ARCH%" == "Win32" (
-    set "CRAFT_PATH=d:\Craft32"
-    set "QT_PATH=%CRAFT_PATH%"
-    set "PATH=%CRAFT_PATH%\bin;%CRAFT_PATH%\dev-utils\bin;%PATH%"
-    set "QT_BIN_PATH=%CRAFT_PATH%\bin"
-    set "QT_PREFIX=%CRAFT_PATH%"
-    exit 1
-)
-
-Rem ******************************************************************************************
-rem Win64 or Win32, VS 2017 or 2019 - note that 2019 generator syntax excludes Arch type,
-rem it's provided by the -A option (OR: CMAKE_GENERATOR_PLATFORM) instead.
-rem QT msvc2017 is still the latest because it actually works for both (2019 compilery is binary compatible)
-set "BUILD_ARCH=Win64"
+set "BUILD_ARCH=%~2"
 set "CMAKE_GENERATOR=Ninja"
 
-set "CRAFT_PATH=d:\Craft64"
-set "QT_PATH=%CRAFT_PATH%"
-set "PATH=%CRAFT_PATH%\bin;%CRAFT_PATH%\dev-utils\bin;%PATH%"
-set "QT_BIN_PATH=%CRAFT_PATH%\bin"
-set "QT_PREFIX=%CRAFT_PATH%"
+if "%BUILD_ARCH%" == "Win32" ( set "CRAFT_PATH=d:\Craft32" )
+if "%BUILD_ARCH%" == "Win32" ( set "QT_PATH=%CRAFT_PATH%" )
+if "%BUILD_ARCH%" == "Win32" ( set "PATH=%CRAFT_PATH%\bin;%CRAFT_PATH%\dev-utils\bin;%PATH%" )
+if "%BUILD_ARCH%" == "Win32" ( set "QT_BIN_PATH=%CRAFT_PATH%\bin" )
+if "%BUILD_ARCH%" == "Win32" ( set "QT_PREFIX=%CRAFT_PATH%" )
 
-
-Rem ******************************************************************************************
+if "%BUILD_ARCH%" == "Win64" ( set "CRAFT_PATH=d:\Craft64" )
+if "%BUILD_ARCH%" == "Win64" ( set "QT_PATH=%CRAFT_PATH%" )
+if "%BUILD_ARCH%" == "Win64" ( set "PATH=%CRAFT_PATH%\bin;%CRAFT_PATH%\dev-utils\bin;%PATH%" )
+if "%BUILD_ARCH%" == "Win64" ( set "QT_BIN_PATH=%CRAFT_PATH%\bin" )
+if "%BUILD_ARCH%" == "Win64" ( set "QT_PREFIX=%CRAFT_PATH%" )
