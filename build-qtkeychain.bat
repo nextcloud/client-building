e5751c6bc64179bdf261e8c36372463c6eef7ecc
alex-z <blackslayer4@gmail.com>
2023-08-18 11:47:07 +0200

    Revert "Revert "Revert "Revert "Revert "Merge pull request #110 from nextcloud/useKdeCraftDependencies"""""
    This reverts commit 43b5c80c23f048bb72fd6b841e8f19d6b8ad739d.

--- build-qtkeychain.bat
+++ build-qtkeychain.bat
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 
 Rem ******************************************************************************************
-rem 			"installer - collect files for Windows 64-bit and/or 32-bit"
+rem 			"qtkeychain - Build for Windows 64-bit and 32-bit"
 Rem ******************************************************************************************
 
 call "%~dp0/defaults.inc.bat" %1
@@ -14,18 +14,13 @@ rem Reference: https://ss64.com/nt/start.html
 
 for %%G in (%BUILD_TARGETS%) do (
     if "%BUILD_TYPE%" == "Debug" (
-        set "DLL_SUFFIX=d"
+        set DLL_SUFFIX=d
     ) else (
-        set "DLL_SUFFIX="
+        set DLL_SUFFIX=
     )
 
-    set "OPENSSL_ROOT_DIR=%OPENSSL_PATH%/%%G"
-    set "ZLIB_PATH=%PROJECT_PATH%/libs/%BUILD_TYPE%/zlib/%%G"
-
-    set "EXTRA_DEPLOY_PATH=%PROJECT_PATH%/deploy-extra/%BUILD_TYPE%/%%G"
-
-    echo "**** collect installer files for %%G (%~nx0)."
-    start "single-build-installer-collect.bat %BUILD_TYPE% %%G" /D "%PROJECT_PATH%/" /B /wait "%~dp0/single-build-installer-collect.bat" %BUILD_TYPE% %%G
+    echo "**** build qtkeychain for %%G (%~nx0)."
+    start "single-build-qtkeychain.bat %BUILD_TYPE% %%G" /D "%PROJECT_PATH%/" /B /wait "%~dp0/single-build-qtkeychain.bat" %BUILD_TYPE% %%G
 
     if !ERRORLEVEL! neq 0 exit !ERRORLEVEL!
 )
