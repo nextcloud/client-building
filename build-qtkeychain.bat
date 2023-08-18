23dee7b68b3287a18e66b57e2704924022ff556d
alex-z <blackslayer4@gmail.com>
2023-08-18 11:47:34 +0200

    Revert "Revert "Merge pull request #113 from nextcloud/buildWithExternalPrebuiltDependencies""
    This reverts commit be350d926d9be9841991633d75c670e14b45765d.

--- build-qtkeychain.bat
+++ build-qtkeychain.bat
@@ -1,30 +1,6 @@
 @echo off
-setlocal EnableDelayedExpansion
 
-Rem ******************************************************************************************
-rem 			"qtkeychain - Build for Windows 64-bit and 32-bit"
-Rem ******************************************************************************************
-
-call "%~dp0/defaults.inc.bat" %1
-
-Rem ******************************************************************************************
-
-rem Reference: https://ss64.com/nt/setlocal.html
-rem Reference: https://ss64.com/nt/start.html
-
-for %%G in (%BUILD_TARGETS%) do (
-    if "%BUILD_TYPE%" == "Debug" (
-        set DLL_SUFFIX=d
-    ) else (
-        set DLL_SUFFIX=
-    )
-
-    echo "**** build qtkeychain for %%G (%~nx0)."
-    start "single-build-qtkeychain.bat %BUILD_TYPE% %%G" /D "%PROJECT_PATH%/" /B /wait "%~dp0/single-build-qtkeychain.bat" %BUILD_TYPE% %%G
-
-    if !ERRORLEVEL! neq 0 exit !ERRORLEVEL!
-)
-
-Rem ******************************************************************************************
+echo "build-qtkeychain.bat is obsolete"
+echo "*** Finished Build: qtkeychain %BUILD_TYPE% %BUILD_ARCH% (GIT_REVISION=%GIT_REVISION%) (%~nx0)"
 
 exit 0
--- common.inc.bat
+++ common.inc.bat
@@ -1,40 +1,18 @@
 Rem ******************************************************************************************
-rem 			"common environment Variables"
+rem "common environment Variables"
 Rem ******************************************************************************************
 
-rem Release or Debug
-set BUILD_TYPE=Release
+set "BUILD_ARCH=%~2"
+set "CMAKE_GENERATOR=Ninja"
 
-if "%~1" == "Debug" (set BUILD_TYPE=%~1)
+if "%BUILD_ARCH%" == "Win32" ( set "CRAFT_PATH=d:\Craft32" )
+if "%BUILD_ARCH%" == "Win32" ( set "QT_PATH=%CRAFT_PATH%" )
+if "%BUILD_ARCH%" == "Win32" ( set "PATH=%CRAFT_PATH%\bin;%CRAFT_PATH%\dev-utils\bin;%PATH%" )
+if "%BUILD_ARCH%" == "Win32" ( set "QT_BIN_PATH=%CRAFT_PATH%\bin" )
+if "%BUILD_ARCH%" == "Win32" ( set "QT_PREFIX=%CRAFT_PATH%" )
 
-Rem ******************************************************************************************
-rem Win64 or Win32, VS 2017 or 2019 - note that 2019 generator syntax excludes Arch type,
-rem it's provided by the -A option (OR: CMAKE_GENERATOR_PLATFORM) instead.
-rem QT msvc2017 is still the latest because it actually works for both (2019 compilery is binary compatible)
-set BUILD_ARCH=Win64
-if	"%VS_VERSION%" == "2017"		set CMAKE_GENERATOR=Visual Studio 15 2017
-if	"%VS_VERSION%" == "2019"		set CMAKE_GENERATOR=Visual Studio 16 2019
-								
-set CMAKE_GENERATOR_PLATFORM=x64								
-set QT_BIN_PATH=%QT_PATH%\msvc%QT_VS_VERSION%_64\bin
-set QT_PREFIX=%QT_PATH%\msvc%QT_VS_VERSION%_64
-set QTKEYCHAIN_PREFIX=%PROJECT_PATH%/libs/%BUILD_TYPE%/qtkeychain/Win64
-
-if "%~2" == "Win32" (set BUILD_ARCH=%~2)
-
-if "%BUILD_ARCH%" == "Win32" (
-    if	"%VS_VERSION%" == "2017"	(	
-		set CMAKE_GENERATOR=Visual Studio 15 2017
-	)
-	if	"%VS_VERSION%" == "2019"	(
-		set CMAKE_GENERATOR=Visual Studio 16 2019
-	)
-	set CMAKE_GENERATOR_PLATFORM=Win32
-    set QT_BIN_PATH=%QT_PATH%\msvc%QT_VS_VERSION%\bin
-	set QT_PREFIX=%QT_PATH%\msvc%QT_VS_VERSION%
-	set QTKEYCHAIN_PREFIX=%PROJECT_PATH%/libs/%BUILD_TYPE%/qtkeychain/Win32
-)
-
-set PATH=%QT_BIN_PATH%;%PATH%
-
-Rem ******************************************************************************************
+if "%BUILD_ARCH%" == "Win64" ( set "CRAFT_PATH=d:\Craft64" )
+if "%BUILD_ARCH%" == "Win64" ( set "QT_PATH=%CRAFT_PATH%" )
+if "%BUILD_ARCH%" == "Win64" ( set "PATH=%CRAFT_PATH%\bin;%CRAFT_PATH%\dev-utils\bin;%PATH%" )
+if "%BUILD_ARCH%" == "Win64" ( set "QT_BIN_PATH=%CRAFT_PATH%\bin" )
+if "%BUILD_ARCH%" == "Win64" ( set "QT_PREFIX=%CRAFT_PATH%" )
