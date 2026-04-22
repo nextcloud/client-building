34134cf8e9454a8d057915b42d993897cff05af9
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2026-04-22 16:14:53 +0200

    chore: update to new zlib binary DLL name
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- single-build-installer-collect.bat
+++ single-build-installer-collect.bat
@@ -187,8 +187,8 @@ start "copy kdsingleapplication-qt6.dll" /D "%MY_COLLECT_PATH%/" /B /wait cp -af
 if %ERRORLEVEL% neq 0 goto
 
 Rem zlib
-echo "* copy zlib1%DLL_SUFFIX%.dll."
-start "copy zlib1%DLL_SUFFIX%.dll" /D "%MY_COLLECT_PATH%/" /B /wait cp -af "%CRAFT_PATH%/bin/zlib1%DLL_SUFFIX%.dll" "%MY_COLLECT_PATH%/"
+echo "* copy z%DLL_SUFFIX%.dll."
+start "copy z%DLL_SUFFIX%.dll" /D "%MY_COLLECT_PATH%/" /B /wait cp -af "%CRAFT_PATH%/bin/z%DLL_SUFFIX%.dll" "%MY_COLLECT_PATH%/"
 if %ERRORLEVEL% neq 0 goto
 
 echo "* copy KArchive files (bin/)."
@@ -275,7 +275,7 @@ if "%USE_CODE_SIGNING%" == "0" (
             "qt6keychain%DLL_SUFFIX%.dll"
             "%LIBCRYPTO_DLL_FILENAME%"
             "%LIBSSL_DLL_FILENAME%"
-            "zlib1%DLL_SUFFIX%.dll"
+            "z%DLL_SUFFIX%.dll"
         ) do (
             start "sign %%~G" /D "%PROJECT_PATH%/" /B /wait %~dp0/sign.bat "%MY_COLLECT_PATH%/%%~G"
 
